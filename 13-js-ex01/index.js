8d625a2334f61ecb55c48a586119b361de7a16bf
andrepavanelli7 <pavanellidev@gmail.com>
2023-02-24 06:34:02 -0300

    estudos 24/02/23

---
+++ 13-js-ex01/index.js
@@ -0,0 +1,14 @@
+function livro (nome, ano, autor) {
+ 
+  return {
+    nome: nome.toUpperCase(),
+    anosDesde: 2023 - ano,
+    frase: nome + ' por ' + autor
+  }
+  
+}
+
+
+const livroRetorno = livro('desaparecido', 1997, 'harlan')
+
+console.log(livroRetorno);
